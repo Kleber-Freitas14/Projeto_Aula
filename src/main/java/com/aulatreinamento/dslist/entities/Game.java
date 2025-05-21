683d68eaaefe5075f7469b75677348c797341485
kleber <kleber.lo.pietro@gmail.com>
2025-05-21 15:47:04 -0300

    2Commit

--- src/main/java/com/aulatreinamento/dslist/entities/Game.java
+++ src/main/java/com/aulatreinamento/dslist/entities/Game.java
@@ -1,17 +1,11 @@
 package com.aulatreinamento.dslist.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Table;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Column;
-
+import jakarta.persistence.*;
 
 import java.util.Objects;
 
 @Entity
-@Table(name = "tb_game")
+@Table(name = "tb_name")
 public class Game {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,25 +16,26 @@ public class Game {
     private Integer year;
     private String genre;
     private String platforms;
-    private Double score;
     private String imgUrl;
-    private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
+    private String shortDescripion;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(){
 
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platforms,
-                Double score, String imgUrl, String shortDecription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms, String imgUrl, String shortDescripion, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
         this.platforms = platforms;
-        this.score = score;
         this.imgUrl = imgUrl;
-        this.shortDescription = shortDecription;
+        this.shortDescripion = shortDescripion;
         this.longDescription = longDescription;
     }
 
@@ -48,79 +43,70 @@ public class Game {
         return id;
     }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public Integer getYear() {
-        return year;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public String getPlatforms() {
-        return platforms;
-    }
-
-    public Double getScore() {
-        return score;
-    }
-
-    public String getImgUrl() {
-        return imgUrl;
-    }
-
-    public String getShortDecription() {
-        return shortDescription;
-    }
-
-    public String getLongDescription() {
-        return longDescription;
-    }
-
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
     }
 
     public void setTitle(String title) {
         this.title = title;
     }
 
+    public Integer getYear() {
+        return year;
+    }
+
     public void setYear(Integer year) {
         this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
     }
 
     public void setGenre(String genre) {
         this.genre = genre;
     }
 
+    public String getPlatforms() {
+        return platforms;
+    }
+
     public void setPlatforms(String platforms) {
         this.platforms = platforms;
     }
 
-    public void setScore(Double score) {
-        this.score = score;
+    public String getImgUrl() {
+        return imgUrl;
     }
 
     public void setImgUrl(String imgUrl) {
         this.imgUrl = imgUrl;
     }
 
-    public void setShortDecription(String shortDecription) {
-        this.shortDescription = shortDecription;
+    public String getShortDescripion() {
+        return shortDescripion;
+    }
+
+    public void setShortDescripion(String shortDescripion) {
+        this.shortDescripion = shortDescripion;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
     }
 
     public void setLongDescription(String longDescription) {
         this.longDescription = longDescription;
     }
-    //Fará a comparação entre dois games se eles são iguais.
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Game game = (Game) o;
+        if (!(o instanceof Game game)) return false;
         return Objects.equals(id, game.id);
     }
 
